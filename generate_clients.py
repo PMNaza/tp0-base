2c2011df594fc6e2e126a7e3adddb4cc4a1543db
PMNaza <taibonazarenogabriel@gmail.com>
2025-08-27 21:57:16 -0300

    fixed generate_compose file

--- generate_clients.py
+++ generate_clients.py
@@ -39,7 +39,7 @@ services:
       - server
     volumes:
       - ./client/config.yaml:/config.yaml
-      - ./data/dataset/agency-{i}.csv:/agency.csv
+      - ./data/agency-{i}.csv:/agency.csv
 """
             )
         # Redes
