08cfd5241901ad356df7de82bd9821cc36015373
PMNaza <taibonazarenogabriel@gmail.com>
2025-08-31 14:50:07 -0300

    final

--- client/common/client.go
+++ client/common/client.go
@@ -73,21 +73,23 @@ func serializeBet() (string, string, string) {
 	return fmt.Sprintf("%s|%s|%s|%s|%s\n", nombre, apellido, documento, nacimiento, numero), documento, numero
 }
 
-// StartClientLoop Send messages to the client until some time threshold is met
 func (c *Client) StartClientLoop() {
-	// There is an autoincremental msgID to identify every message sent
-	// Messages if the message amount threshold has not been surpassed
+
 	for msgID := 1; msgID <= c.config.LoopAmount; msgID++ {
-		// Create the connection the server in every loop iteration. Send an
+
 		c.createClientSocket()
 
-		// TODO: Modify the send to avoid short-write
 		msg, documento, numero := serializeBet()
 		totalSent := 0
 		for totalSent < len(msg) {
 			n, err := c.conn.Write([]byte(msg)[totalSent:])
 			if err != nil {
-				// handle error
+				log.Errorf("action: send_message | result: fail | client_id: %v | error: %v",
+					c.config.ID,
+					err,
+				)
+				c.conn.Close()
+				return
 			}
 			totalSent += n
 		}
@@ -110,7 +112,6 @@ func (c *Client) StartClientLoop() {
 
 		log.Infof("action: apuesta_enviada | result: success | dni: %s | numero: %s", documento, numero)
 
-		// Wait a time between sending one message and the next one
 		time.Sleep(c.config.LoopPeriod)
 
 	}
